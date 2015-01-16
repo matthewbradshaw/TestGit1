cd7580fc5124f42a09e32d7b093acfecae45effa
matthewbradshaw <matthew.m.bradshaw@gmail.com>
2015-01-16 17:40:12 -0600

    Initial Commit

---
+++ TestGit1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit1
+//
+//  Created by Matthew Bradshaw on 1/16/15.
+//  Copyright (c) 2015 Matthew Bradshaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit1
+//
+//  Created by Matthew Bradshaw on 1/16/15.
+//  Copyright (c) 2015 Matthew Bradshaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
